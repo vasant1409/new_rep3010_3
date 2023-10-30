ec75f1f4022a64c9202f6c9073485ae0bb480caf
vasant1409 <149039305+vasant1409@users.noreply.github.com>
2023-10-30 18:57:06 +0530

    Update sample2_stg2.sql

--- models/example/sample2_stg2.sql
+++ models/example/sample2_stg2.sql
@@ -1,3 +1,4 @@
-WITH  survey2 as (select * from `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`), y as (select year,industry_name,variable,Value from survey2)
+WITH  survey2 as (select * from `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`), y as 
+  (select year,industry_name_ANZSIC,variable,Value from survey2)
 
 Select * from y
