f60e1fe5f2de2463ad8ff982a944917f55e79e39
vasant1409 <149039305+vasant1409@users.noreply.github.com>
2023-10-30 18:24:22 +0530

    Update stg_annual_enterprise_survey02.sql

--- models/example/stg_annual_enterprise_survey02.sql
+++ models/example/stg_annual_enterprise_survey02.sql
@@ -1,1 +1,18 @@
-token-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
+WITH
+survey AS (
+SELECT * FROM `cloudside-academy.dbt_sample_data.annual-enterprise-survey02`),
+
+n AS ( 
+SELECT 
+    id,
+    year AS Year,
+    industry_code_ANZSIC AS Industry_code,
+    industry_name_ANZSIC AS Industry_name,
+    rme_size_grp AS Size_group,
+    variable AS Variable,
+    value,
+    unit AS Units
+FROM survey
+)
+
+SELECT * FROM n
